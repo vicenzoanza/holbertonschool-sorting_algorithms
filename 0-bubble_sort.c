ecf3e3894926a409dc6c2a733af64d6a16bd4da7
lucassoriabusto <lucassoriabusto@hotmail.com>
2023-05-19 12:39:07 -0700

    Task 00

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -11,10 +11,11 @@ void bubble_sort(int *array, size_t size)
 {
 	size_t i, j = 0;
 	int aux;
-	for(i = 0; i < size; i++)
+
+	for (i = 0; i < size; i++)
 	{
-		for(j = 0; j < size - 1; j++)
-			if(array[j] > array[j + 1])
+		for (j = 0; j < size - 1; j++)
+			if (array[j] > array[j + 1])
 			{
 				aux = array[j];
 				array[j] = array[j + 1];
