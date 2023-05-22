6b7287b801d0226ec549634ae2f57ff0ec00780d
vicenzoanza <viceanza2010@hotmail.com>
2023-05-22 10:59:45 -0700

    betty

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -9,8 +9,8 @@ void insertion_sort_list(listint_t **list)
 {
 listint_t *sorted, *current, *temp;
 
-if (!list || !(*list) || !(*list)->next)
-	return;
+	if (!list || !(*list) || !(*list)->next)
+		return;
 
 	for (current = (*list)->next; current != NULL; current = temp)
 	{
