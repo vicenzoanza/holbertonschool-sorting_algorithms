2d0e505645dc50f25cfb1eb4ecafd38a8827b9a6
vicenzoanza <viceanza2010@hotmail.com>
2023-05-28 11:35:51 -0700

    sol

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,7 +1,7 @@
 #include "sort.h"
 
 /**
-  * quick_sort - sorts an array of integers in ascending order using the 
+  * quick_sort - sorts an array of integers in ascending order using the
   * Quick sort algorithm.
   * @array: array.
   * @size: size.
